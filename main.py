56deff05334bbed6ffeb68a63d18b509e6a91323
rodygayz12 <161971854+rodygayz12@users.noreply.github.com>
2024-04-19 13:04:18 +0400

    Update main.py

--- main.py
+++ main.py
@@ -90,4 +90,4 @@ if os.path.exists("/content/Telegram-Leecher/my_bot.session"):
     
 print("\rStarting Bot....")
 
-!cd /content/Telegram-Leecher/ && python3 -m colab_leecher #type:ignore
+!cd /content/Telegram-Leecher/ && python3 -m colab_leecher
